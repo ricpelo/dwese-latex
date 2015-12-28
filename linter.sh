b8dde421eab5e275b08e749b1f6d8d3049e5992a
Ricardo Pérez <ricpelo@gmail.com>
2015-12-28 23:12:10 +0100

    Mejoras en el linter

--- linter.sh
+++ linter.sh
@@ -4,7 +4,13 @@ fn_php()
 {
     mv $1.php $1
     echo "<?php\n" | cat - $1 > $1.php
-    php -l $1.php 2> $1.linter
+    > $1.linter
+    fn_iphp $1
+}
+
+fn_iphp()
+{
+    php -l $1.php 2>> $1.linter
     php -l $1.php
 }
 
@@ -12,15 +18,21 @@ fn_htmlphp()
 {
     tidy -eq $1.php 2> $1.linter
     tidy -eq $1.php
+    fn_iphp $1
 }
 
 cd code
-if [ "$1" = "php" ]
-then
-    fn_php $2
-elif [ "$1" = "html+php" ]
-then
-    fn_htmlphp $2
-fi
+
+case $1 in
+    php)
+        fn_php $2;;
+    html+php)
+        fn_htmlphp $2;;
+    iphp)
+        fn_iphp $2;;
+    *)
+        ;;
+esac
+
 cd ..
 
