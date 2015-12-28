d4a4940565ecb3a37a6d6092fe3644017e48278d
Ricardo Pérez <ricpelo@gmail.com>
2015-12-28 10:42:31 +0100

    Mejora en el linter

--- linter.sh
+++ linter.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
+fn_php()
+{
+    mv $1.php $1
+    echo "<?php\n" | cat - $1 > $1.php
+    php -l $1.php 2> $1.linter
+    php -l $1.php
+}
+
 cd code
-mv $1.php $1
-echo "<?php\n" | cat - $1 > $1.php
-php -l $1.php 2> $1.linter
-php -l $1.php
+if [ "$1" = "php" ]
+then
+    fn_php $2
+fi
 cd ..
 
