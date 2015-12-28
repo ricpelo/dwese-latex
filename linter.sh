3c65e1f2281a80548a6a82830cebeb7cfb7e3ba3
Ricardo Pérez <ricpelo@gmail.com>
2015-12-28 19:47:59 +0100

    Añade el linter tidy para los html+php

--- linter.sh
+++ linter.sh
@@ -8,10 +8,19 @@ fn_php()
     php -l $1.php
 }
 
+fn_htmlphp()
+{
+    tidy -eq $1.php 2> $1.linter
+    tidy -eq $1.php
+}
+
 cd code
 if [ "$1" = "php" ]
 then
     fn_php $2
+elif [ "$1" = "html+php" ]
+then
+    fn_htmlphp $2
 fi
 cd ..
 
